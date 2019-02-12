0a521bb1277f5ddd5f7b22d69dd9d9a188fe6de4
Chris Herbert <herbert@bermanco.com>
2019-02-11 21:58:32 -0500

    Automatically handle wordpress.com files directories

--- app/DotComUrl.php
+++ app/DotComUrl.php
@@ -10,7 +10,22 @@ class DotComUrl extends Url {
 		$parse = parse_url($this->clean_url);
 		$host = $parse['host'];
 
+		// Remove `.files.` - this is where .com sites stores their files, rather than the usual /uploads directory
+
+
 		return "https://public-api.wordpress.com/rest/v1.1/sites/$host/comments";
+
+	}
+
+	/**
+	 * Remove '.files.' from site urls
+	 * @return string  Site url
+	 */
+	protected function clean_url($url){
+
+		$url = parent::clean_url($url);
+
+		return str_replace('.files.', '.', $url);
 
 	}
 
